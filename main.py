5c002faefd993a24a58705c2997d7fe5421867ad
Liam <liamrbelh@gmail.com>
2025-09-15 11:02:21 -0500

    Done

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from stats import get_num_words
+from stats import get_letter_frequency
+from stats import sort_dict
+
+import sys
 
 def get_book_text(file):
     with open(file) as f:
@@ -6,10 +10,27 @@ def get_book_text(file):
         pass
     return file_contents
 
-def main():
-    file = 'books/frankenstein.txt'
+def book_report(file):
     file_string = get_book_text(file)
     num_words = get_num_words(file_string)
-    print(str(num_words) + " words found in the document")
+    letter_frequency = get_letter_frequency(file_string)
+    sorted_dict = sort_dict(letter_frequency)
+
+    print("========== BOOKBOT ==========")
+    print("Analyzing book found at " + file + "...")
+    print("---------- Word Count ----------")
+    print("Found " + str(num_words) + " total words")
+    print("---------- Character Count ----------")
+    for letter, val in sorted_dict.items():
+        if letter.isalpha():
+            print(str(letter) + ": " + str(val))
+    print("========== END ==========")
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book_report(sys.argv[1])
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,19 @@
 def get_num_words(text):
     num_words = text.split()
     return len(num_words)
+
+def get_letter_frequency(text):
+    letter_dict = {}
+    for letter in text:
+        if letter.lower() not in letter_dict:
+            letter_dict[letter.lower()] = 1
+        else:
+            letter_dict[letter.lower()] += 1
+    return letter_dict
+
+def sort_on(items):
+    return items['num']
+
+def sort_dict(my_dict):
+    sorted_dict = dict(sorted(my_dict.items()))
+    return sorted_dict
